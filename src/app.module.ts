9566a2d099aa034369b4b78ea3aa5d1b0a1d8dc9
Derick Benites <dericksantos1995@hotmail.com>
2023-04-01 13:34:00 -0300

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './configs/typeorm.config';
+
+@Module({
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
