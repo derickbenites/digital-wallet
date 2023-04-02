008652d3339215987bad5555d705635fb58075b6
Derick Benites <dericksantos1995@hotmail.com>
2023-04-02 14:15:45 -0300

    improvements in folders and organization

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controller';
 import { UsersRepository } from './repositories/users.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
